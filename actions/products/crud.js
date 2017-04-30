c420429af8882749dbae82633d681e6480f198e1
berangere.la.touche <berangere.lt@gmail.com>
2017-04-30 15:26:38 +0200

    products clear

--- actions/products/crud.js
+++ actions/products/crud.js
@@ -4,8 +4,8 @@ module.exports = (api) => {
   const Category = api.models.Category;
 
   function create(req, res, next) {
-    const userId = req.seller;
-    const categoryId = req.category;
+    const userId = req.body.seller;
+    const categoryId = req.body.category;
 
     let products = new Product(req.body);
     products.seller = userId;
@@ -20,21 +20,23 @@ module.exports = (api) => {
       if (found) {
         return res.status(401).send('product.already.exists');
       }
-    });
 
-    products.save((err, data) => {
-      if (err) {
-        return res.status(500).send(err);
+      else {
+        products.save((err, data) => {
+          if (err) {
+            return res.status(500).send(err);
+          }
+
+          Category.findById(categoryId, (err, user) => {
+            if (err) {
+              return res.status(500).send(err);
+            }
+
+            products.category.push(data._id.toString());
+              return res.send(data);
+          });
+        });
       }
-
-      Category.findById(categoryId, (err, user) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-
-        products.category.push(data._id.toString());
-        return res.send(data);
-      });
     });
   }
 
@@ -63,13 +65,12 @@ module.exports = (api) => {
     });
   }
 
-  function findAllByCategory(req, res, next) {
-    Product.findAll({
-      category: req.category,
-    }, (err, data) => {
+  function sortByDescPrice(req, res, next) {
+    Product.find({}).sort('-price').exec(function(err, data) {
       if (err) {
         return res.status(500).send(err);
       }
+
       if (!data || data.length == 0) {
         return res.status(204).send(data);
       }
@@ -77,10 +78,61 @@ module.exports = (api) => {
     });
   }
 
+  function sortByAscPrice(req, res, next) {
+    Product.find({}).sort('price').exec(function(err, data) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      if (!data || data.length == 0) {
+        return res.status(204).send(data);
+      }
+      return res.send(data);
+    });
+  }
+
+  function sortByDescDate(req, res, next) {
+    Product.find({}).sort('-date').exec(function(err, data) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      if (!data || data.length == 0) {
+        return res.status(204).send(data);
+      }
+      return res.send(data);
+    });
+  }
+
+  function sortByAscDate(req, res, next) {
+    Product.find({}).sort('date').exec(function(err, data) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      if (!data || data.length == 0) {
+        return res.status(204).send(data);
+      }
+      return res.send(data);
+    });
+  }
+
+
+  function findAllByCategory(req, res, next) {
+    Product.find({category: req.params.id}, (err, data) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      if (!data || data.length == 0) {
+        return res.status(204).send(data);
+      }
+      console.log(data);
+      return res.send(data);
+    });
+  }
+
   function findAllBySeller(req, res, next) {
-    Product.findAll({
-      seller: req.seller,
-    }, (err, data) => {
+    Product.find({seller: req.params.id}, (err, data) => {
       if (err) {
         return res.status(500).send(err);
       }
@@ -115,64 +167,17 @@ module.exports = (api) => {
     });
   }
 
-  function assign(req, res, next) {
-      Product.findByIdAndUpdate(req.params.id, {
-              assigned: req.body.category
-          },
-          (err, products) => {
-            console.log(req.params.id);
-            console.log(req.body.category);
-              if (err) {
-                  return res.status(500).send(err);
-              }
-              res.send(products);
-          });
-  };
-
-    // function saveProduct() {
-    //   products.save((err, data) => {
-    //     if (err) {
-    //       return res.status(500).send(err);
-    //     }
-    //
-    //     return res.send(data);
-    //   });
-    // };
-
-  function addCategory(req, res, next) {
-      let category = new Category(req.body);
-
-      Category.findById(req.params.id, (err, data) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-
-        if (!data) {
-          return res.status(204).send(data);
-        }
-        return updateCategory();
-      });
-
-      function updateCategory() {
-        Product.findByIdAndUpdate(req.params.id, req.body, {new : true}, (err, data) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-          if (!data) {
-            return res.status(204).send(data);
-          }
-          return res.send(data);
-        });
-      }
-    }
-
   return {
     create,
     findOne,
     findAll,
+    sortByDescPrice,
+    sortByAscPrice,
+    sortByDescDate,
+    sortByAscDate,
+    findAllByCategory,
+    findAllBySeller,
     update,
-    remove,
-    assign,
-    addCategory
+    remove
   };
 }
--- routes/products.js
+++ routes/products.js
@@ -4,30 +4,40 @@ module.exports = (api) => {
   router.get('/',
     api.actions.products.findAll);
 
-  router.get('/:id',
-      api.actions.products.findOne);
+  router.get('/descprice',
+      api.actions.products.sortByDescPrice);
 
+  router.get('/ascprice',
+      api.actions.products.sortByAscPrice);
+
+  router.get('/descdate',
+      api.actions.products.sortByDescDate);
+
+  router.get('/ascdate',
+      api.actions.products.sortByAscDate);
+
+  router.get('/:id',
+    api.actions.products.findOne);
+
+  router.get('/categories/:id',
+      api.middlewares.bodyParser.json(),
+      api.actions.products.findAllByCategory);
+
+  router.get('/seller/:id',
+      api.middlewares.bodyParser.json(),
+      api.actions.products.findAllBySeller);
 
   router.post('/',
-      api.middlewares.bodyParser.json(),
-      api.middlewares.ensureAuthenticated,
-      api.actions.products.create);
+    api.middlewares.bodyParser.json(),
+    //api.middlewares.ensureAuthenticated,
+    api.actions.products.create);
 
   router.put('/:id',
-      api.middlewares.bodyParser.json(),
-      api.actions.products.update);
+    api.middlewares.bodyParser.json(),
+    api.actions.products.update);
 
   router.delete('/:id',
-      api.actions.products.remove);
-
-  router.put('/:id/category',
-      api.middlewares.bodyParser.json(),
-      api.actions.products.addCategory
-  )
-  /*router.post('/:id/seller',
-      api.middlewares.bodyParser.json(),
-      //api.middlewares.ensureAuthenticated,
-      api.actions.products.assign);*/
+    api.actions.products.remove);
 
   return router;
 }
